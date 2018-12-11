6f13c2fd4b0ea7f8bdfadd056ba9a9b9b6e99307
taoweijun <1213359280@qq.com>
2018-12-11 23:50:05 +0800

    第二次提交品优购工程

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -4,8 +4,10 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,6 +28,12 @@ public class BrandController{
     public List<TbBrand> testPage(Integer page,Integer rows){
         //return brandService.testPage(page, rows);
         return (List<TbBrand>) brandService.findPage(page, rows).getRows();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page",defaultValue = "1")Integer page,
+                               @RequestParam(value = "rows",defaultValue = "10")Integer rows){
+        return brandService.findPage(page, rows);
     }
 
 
