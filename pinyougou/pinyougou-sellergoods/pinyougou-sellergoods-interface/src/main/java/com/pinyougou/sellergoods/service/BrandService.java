109c7bed3064ca1bc40ff55ea80bfab338a713c1
taoweijun <1213359280@qq.com>
2018-12-11 09:49:32 +0800

    第二次提交品优购工程

--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,9 +1,15 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.servlce.BaseService;
 
+
+import java.io.Serializable;
 import java.util.List;
 
-public interface BrandService {
+public interface BrandService extends BaseService<TbBrand> {
     public List<TbBrand> queryAll();
+
+    List<TbBrand> testPage(Integer page,Integer rows);
+
 }
--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,19 +1,30 @@
 package com.pinyougou.sellergoods.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.service.impl.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+
+//将服务暴露注册到dubbo注册中心
 @Service(interfaceClass = BrandService.class)
-public class BrandServiceImpl implements BrandService {
+public class BrandServiceImpl extends BaseServiceImpl<TbBrand> implements BrandService {
     @Autowired
     private BrandMapper brandMapper;
 
     @Override
     public List<TbBrand> queryAll() {
         return brandMapper.queryAll();
+    }
+
+    @Override
+    public List<TbBrand> testPage(Integer page, Integer rows) {
+        //设置分页
+        PageHelper.startPage(page,rows);
+        return brandMapper.selectAll();
     }
 }
